3f44ad41a9cc47b55260399b4a8c1d23a5dc6ee6
Mohsin370 <mohsinijaz13@gmail.com>
2024-01-14 23:59:17 +1030

    landing page text update

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -3,7 +3,6 @@ import landingPotrait from "../../assets/images/Malika.png";
 import insta from "../../assets/icons/insta.png";
 import arrow from "../../assets/icons/arrow.png";
 import email from "../../assets/icons/email.png";
-import { motion } from "framer-motion";
 
 const leftSection = () => {
   return (
@@ -11,29 +10,34 @@ const leftSection = () => {
       <div className={styles.textContainer}>
         <h1 className="">Malika Riaz</h1>
         <h3>Visual Artist</h3>
-        <h2>
+        <h4>
           "Let your imagination be the canvas, dreams the paint, and courage the
           brush. In the gallery of life, create a masterpiece that reflects the
           vibrant hues of your passion and the bold strokes of your resilience.
           Every stroke tells a story, and every color whispers, You are an
           artist of your destiny."
-        </h2>
+        </h4>
       </div>
-      
-      <div className={styles.catalougeText}>
-        <p>Catalouge</p>
-        <img src={arrow} alt="arrow" name="arrow"></img>
-      </div>
-      <div className="d-flex mt-3 justify-content-center">
-        <a
-          target="_blank"
-          href="https://www.instagram.com/malikariaz_/"
-          className={styles.contact}
-        >
-          <img src={insta} alt="Instagram" name="insta" ></img>
-        </a>
-        <div className={styles.contact} onClick={() => window.location = 'mailto:malikariaz99@gmail.com'}>
-          <img src={email} alt="email"  className="pl-1"  name="email"></img>
+
+      <div className="position-absolute bottom-0 start-50 translate-middle">
+        <div className={styles.catalougeText}>
+          <p>Catalouge</p>
+          <img src={arrow} alt="arrow" name="arrow"></img>
+        </div>
+        <div className="d-flex mt-3 justify-content-center">
+          <a
+            target="_blank"
+            href="https://www.instagram.com/malikariaz_/"
+            className={styles.contact}
+          >
+            <img src={insta} alt="Instagram" name="insta"></img>
+          </a>
+          <div
+            className={styles.contact}
+            onClick={() => (window.location = "mailto:malikariaz99@gmail.com")}
+          >
+            <img src={email} alt="email" className="pl-1" name="email"></img>
+          </div>
         </div>
       </div>
     </div>
