0e7449302acbf6cddc5b06a6ce1cc87825d86f12
Khawaja Mohsin <62910365+Mohsin370@users.noreply.github.com>
2024-01-17 09:49:52 +1030

    Update artWork.jsx

--- src/components/artWork/artWork.jsx
+++ src/components/artWork/artWork.jsx
@@ -3,7 +3,7 @@ import styles from "./artWork.module.css";
 function ArtWork(props) {
   return (
     <>
-      <img className={styles.image} src={props.image} loading="lazy"></img>
+      <img className={styles.image} src={props.image} loading="lazy" alt="Art Work Display Image" ></img>
       <p>{props.name}</p>
     </>
   );
