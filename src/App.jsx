a104d3d6525690af965e02423580f4bb77b558d9
Mohsin370 <mohsinijaz13@gmail.com>
2024-01-21 15:23:31 +1030

    add some routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Catalouge from './pages/catalouge/catalouge';
-import Landing from './pages/landing/landing';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Catalouge from "./pages/catalouge/catalouge";
+import Landing from "./pages/landing/landing";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header/header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>
-  <Landing></Landing>
-  <Catalouge></Catalouge>
-  </>;
+  return (
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/catalouge" element={<Catalouge />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
