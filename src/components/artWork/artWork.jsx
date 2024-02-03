651bc9882ea797c8293edab8c9146362521eaab4
Mohsin370 <mohsinijaz13@gmail.com>
2024-02-03 14:45:29 +1030

    ease animation added

--- src/components/artWork/artWork.jsx
+++ src/components/artWork/artWork.jsx
@@ -2,7 +2,7 @@ import styles from "./artWork.module.css";
 
 function ArtWork(props) {
   return (
-    <div role="button">
+    <div role="button" className={styles.imgContainer}>
       <img className={styles.image} src={props.image} loading="lazy" alt="Art Work Display Image" ></img>
       <p>{props.name}</p>
     </div>
