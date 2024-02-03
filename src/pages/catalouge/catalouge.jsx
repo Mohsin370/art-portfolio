846e4186198590fd279bdd72f9d827d731e2efd5
Mohsin370 <mohsinijaz13@gmail.com>
2024-02-03 15:01:40 +1030

    navigate back on catalouge page

--- src/pages/catalouge/catalouge.jsx
+++ src/pages/catalouge/catalouge.jsx
@@ -1,8 +1,8 @@
-
-import {Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import ArtWork from "../../components/artWork/artWork";
 import { useNavigate } from "react-router-dom";
 import artWorks from "../../helper/artWorkList";
+import arrowLeft from "../../assets/icons/arrow-left.svg";
 
 function Catalouge() {
   let navigate = useNavigate();
@@ -23,6 +23,12 @@ function Catalouge() {
 
   return (
     <Container className="pb-5  mt-5 pt-5">
+      <div>
+        <h3 className="p-3" role="button" onClick={() => navigate(`/`)}>
+          <img className="pb-1" src={arrowLeft} />
+          Catalouge
+        </h3>
+      </div>
       <div className=" d-flex flex-wrap gap-3 justify-content-center  p-2 ">
         {artRenderer()}
       </div>
